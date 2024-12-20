d6784c0ed82365052b5f81575cbb519a9dfc3f10
Debajyati Dey <ddebajyati@gmail.com>
2024-12-21 01:18:56 +0530

    feat: generator to write the JSON file using nodejs streams API

--- fileOprt.js
+++ fileOprt.js
@@ -18,10 +18,36 @@ async function getPath(filePath) {
   return path.join(dirname, filePath);
 }
 
+function* jsonGenerator(data) {
+  yield '[';
+  for (let i = 0; i < data.length; i++) {
+    yield JSON.stringify(data[i], null, 2);
+    if (i < data.length - 1) {
+      yield ',';
+    }
+  }
+  yield ']';
+}
+
 async function writeJSON(filePath, data) {
-  const { default: fs } = await import('node:fs');
-  const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(filePath, jsonData);
+  const { default: fs } = await import("node:fs");
+  // const jsonData = JSON.stringify(data, null, 2);
+  const jsonData = jsonGenerator(data);
+  try {
+    return new Promise((resolve, reject) => {
+      const writeStream = fs.createWriteStream(filePath);
+      writeStream.on("error", (err) => {
+        reject(new Error(`Error writing JSON file: ${err.message}`));
+      });
+      writeStream.on("finish", resolve);
+      for (const chunk of jsonData) {
+        writeStream.write(chunk);
+      }
+      writeStream.end();
+    });
+  } catch (err) {
+    throw new Error(`Error writing JSON file: ${err.message}`);
+  }
 }
 
 export { readLogFile, getPath, writeJSON };
