86bf1ba4bfb1da84af7fa91ade017763d52ec31b
Debajyati Dey <ddebajyati@gmail.com>
2024-12-20 23:02:42 +0530

    moving all utility functions to seperate module files

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import parseLogFile from './logParser.js';
+// import { argv } from 'process';
+import { getPath, writeJSON } from './fileOprt.js';
 
-async function getPath(filePath) {
-  const { default: path } = await import('node:path');
-  const dirname  = import.meta.dirname;
-  return path.join(dirname, filePath);
-}
-
-async function writeJSON(filePath, data) {
-  const { default: fs } = await import('node:fs');
-  const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(filePath, jsonData);
-}
-
+// main function
 await (async () => {
-  const logFilePath = await getPath("WindowsUpdate.log");
-  const parsedLogs = await parseLogFile(logFilePath);
-  await writeJSON("parsedLogs.json", parsedLogs);
-  console.log("Done!");
-  console.log("Parsed logs written to parsedLogs.json");
+  try {
+    const logFilePath = await getPath("WindowsUpdate.log");
+    const parsedLogs = await parseLogFile(logFilePath);
+
+    await writeJSON("parsedLogs.json", parsedLogs);
+
+    console.log("Done!");
+    console.log("Parsed logs written to parsedLogs.json");
+  } catch (err) {
+    console.error(`Error parsing log file: ${err}`);
+    process.exit(1);
+  }
 })();
